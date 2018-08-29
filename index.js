a9cfcb22a2a4b6bc22b0c2ee1450ac40611c618d
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-29 23:04:43 +0800

    ..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const hostname = '127.0.0.1';
-const POST = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer((request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/plain');
